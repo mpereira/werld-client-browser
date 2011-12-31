354bd86f2e8a000fd8951fc8e3654f2ef9161e2e
Murilo Pereira <murilo@murilopereira.com>
2011-12-31 20:25:19 -0200

    This is a little smarter.

--- public/javascripts/werld.js
+++ public/javascripts/werld.js
@@ -1,10 +1,11 @@
 var Werld = {
   config: {},
-  account: {},
+  account: {
+    provider: {}
+  },
   setAccount: function(providerName, apiResponse) {
     this.account.name = apiResponse.name;
     this.account.email = apiResponse.email;
-    this.account.provider = {};
     this.account.provider.id = apiResponse.id;
     this.account.provider.name = providerName;
   }
