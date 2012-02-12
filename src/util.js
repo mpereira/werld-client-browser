92671ba73a188f7e9df4d2e0f4e947883a73ce8c
Murilo Pereira <murilo@murilopereira.com>
2012-02-11 22:40:35 -0200

    Unused.

--- src/util.js
+++ src/util.js
@@ -20,25 +20,3 @@ Werld.util = {
              Werld.Config.PIXELS_PER_TILE);
   }
 };
-
-Werld.Util.Queue = function() {
-  this.elements = [];
-};
-
-Werld.Util.Queue.prototype = {
-  enqueue: function(element) {
-    this.elements.unshift(element);
-  },
-  dequeue: function() {
-    this.elements.pop();
-  },
-  forEach: function(lambda) {
-    this.elements.forEach(lambda);
-  },
-  reduceRight: function(lambda) {
-    this.elements.reduceRight(lambda);
-  },
-  size: function() {
-    return(this.elements.length);
-  }
-};
