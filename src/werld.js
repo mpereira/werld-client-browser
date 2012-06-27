0bcedcac2c7fc6bd532ea8193b1bdc5eb2467d79
Murilo Pereira <murilo@murilopereira.com>
2012-06-26 21:04:34 -0300

    Fixed.

--- src/werld.js
+++ src/werld.js
@@ -246,7 +246,7 @@ var Werld = {
     var stats = new Stats();
     stats.setMode(0);
 
-    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.position = 'fixed';
     stats.domElement.style.left = '0px';
     stats.domElement.style.top = '0px';
 
